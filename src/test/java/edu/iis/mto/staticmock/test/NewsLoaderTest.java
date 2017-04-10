3d4477823f942df98ac87525c87dca2d740a66bc
Jakub Olczak <kuba.olczak.jo@gmail.com>
2017-04-10 23:46:41 +0200

    Aktualizacja setUp i importów

--- src/test/java/edu/iis/mto/staticmock/test/NewsLoaderTest.java
+++ src/test/java/edu/iis/mto/staticmock/test/NewsLoaderTest.java
@@ -4,11 +4,36 @@ import static org.junit.Assert.*;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+
+import edu.iis.mto.staticmock.Configuration;
+import edu.iis.mto.staticmock.ConfigurationLoader;
+import edu.iis.mto.staticmock.NewsReaderFactory;
+import edu.iis.mto.staticmock.PublishableNews;
+
+import static org.powermock.api.mockito.PowerMockito.*;
+import org.mockito.internal.util.reflection.*;
+import static org.hamcrest.CoreMatchers.*;
+
+@RunWith(PowerMockRunner.class)
+@PrepareForTest({ConfigurationLoader.class, NewsReaderFactory.class, PublishableNews.class})
 
 public class NewsLoaderTest {
-
+	
+	private ConfigurationLoader testConfigurationLoader = null;
+	
 	@Before
 	public void setUp() throws Exception {
+		testConfigurationLoader = PowerMockito.mock(ConfigurationLoader.class);
+		PowerMockito.mockStatic(ConfigurationLoader.class);
+		PowerMockito.when(ConfigurationLoader.getInstance()).thenReturn(testConfigurationLoader);
+		
+		Configuration testConfiguration = new Configuration();
+		Whitebox.setInternalState(testConfiguration, "readerType", "testNewsReader");
+		when(testConfigurationLoader.loadConfiguration()).thenReturn(testConfiguration);
 	}
 
 	@Test
